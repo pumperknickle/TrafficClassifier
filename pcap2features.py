fdd4b45ef5c7606f17fb99f18bd61e2d3f61b3e0
Pumper Knickle <pumperknickle@protonmail.com>
2020-01-30 14:21:12 -0800

    LSTM

--- pcap2features.py
+++ pcap2features.py
@@ -78,5 +78,34 @@ for path in paths:
       features.append(fv)
       labels.append(counter)
   counter = counter + 1
-print(features)
-print(labels)        
+
+import numpy as np
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+
+dummy_labels = np_utils.to_categorical(labels)
+num_classes = len(dummy_labels[0])
+num_features = len(features[0])
+X = np.array(features)
+
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(8, input_dim=num_features, activation='relu'))
+	model.add(Dense(num_classes, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=1)
+kfold = KFold(n_splits=10, shuffle=True)
+results = cross_val_score(estimator, X, dummy_labels, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
---
+++ pcapSequenceClassifier.py
@@ -0,0 +1,72 @@
+from scapy.all import *
+from itertools import groupby
+import sys
+import glob
+from featureTransforms import convert_to_features
+
+directory = sys.argv[1]
+extended = directory + '/*/'
+paths = glob.glob(extended)
+sequence_length = 100
+
+def split_sequence_unevenly(lst, n):
+  for i in range(0, len(lst), n):
+     yield lst[i:i + n]
+
+def split_sequence_evenly(lst, n):
+  split = list(split_sequence_unevenly(lst, n))
+  split.pop()
+  return split
+
+def get_sequences(packets):
+  sequences = []
+  for i in range(len(packets)-1):
+    next_packet_size = len(packets[i+1])
+    next_packet_interval = packets[i+1].time - packets[i].time
+    sequences.append([next_packet_size, next_packet_interval])
+  return sequences
+
+counter = 0
+features = []
+labels = []
+
+for path in paths:
+  pcapPath = path + '/*.pcap'
+  pcapFiles = glob.glob(pcapPath)
+  for file in pcapFiles:
+    pcap = rdpcap(file)
+    sequences = get_sequences(pcap)
+    chunks = split_sequence_evenly(sequences, sequence_length)
+    for chunk in chunks:
+      features.append(chunk)
+      labels.append(counter)
+  counter += 1
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from keras.layers import LSTM
+
+dummy_labels = np_utils.to_categorical(labels)
+num_classes = len(dummy_labels[0])
+
+def baseline_model():
+  model = Sequential()
+  model.add(LSTM(32, input_shape=(sequence_length, 2)))
+  model.add(Dense(num_classes, activation='softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+  return model
+
+feature_array = np.array(features)
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=1)
+kfold = KFold(n_splits=10, shuffle=True)
+results = cross_val_score(estimator, feature_array, dummy_labels, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+
