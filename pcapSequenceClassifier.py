44d87709d8e5723a7ecec479da5a31a1f75ea7af
Pumper Knickle <pumperknickle@protonmail.com>
2020-02-07 16:17:46 -0800

    double lstm

--- pcapSequenceClassifier.py
+++ pcapSequenceClassifier.py
@@ -43,6 +43,7 @@ for path in paths:
   counter += 1
 
 import numpy as np
+from keras.layers import Dropout
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -58,10 +59,16 @@ num_classes = len(dummy_labels[0])
 
 def baseline_model():
   model = Sequential()
-  model.add(LSTM(100, input_shape=(sequence_length, 2)))
+  model.add(LSTM(100, input_shape=(sequence_length, 2), return_sequences=True))
+  model.add(Dropout(0.2))
+  model.add(LSTM(100))
+  model.add(Dropout(0.2))
   model.add(Dense(num_classes, activation='softmax'))
   model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
   return model
+
+from keras.utils import plot_model
+plot_model(baseline_model(), to_file='model.png', show_shapes=True, show_layer_names=True)
 
 print(features)
 print(labels)
@@ -70,5 +77,4 @@ estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, v
 kfold = KFold(n_splits=10, shuffle=True)
 results = cross_val_score(estimator, feature_array, dummy_labels, cv=kfold)
 print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
-
 
